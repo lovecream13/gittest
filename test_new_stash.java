8959b25ae29273decdd59674373fb9827ef996f6
lovecream13 <Li13870573772@163.com>
2025-08-24 23:17:17 +0800

    "hello new_stash stash"

---
+++ test_new_stash.java
@@ -0,0 +1,5 @@
+public class test_new_stash {
+    public static void main(String[] args) {
+        System.out.println("test new stash");
+    }
+}
--- test_stash.java
+++ test_stash.java
@@ -1,7 +1,5 @@
-public class hello {
+public class test_stash {
     public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("test");
         System.out.println("test stash");
         System.out.println("test stash");
         System.out.println("test stash");
